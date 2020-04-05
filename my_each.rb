6a5c42907a06f06c27886d45742f5aedf75df2a8
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-05 23:26:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 def my_each(x)
   if x.length = 0
-  i = 0
-  while i < x.length
-    yield x[i]
-    i = i + 1
-  end
-  binding.pry
-  x
+    i = 0
+    while i < x.length
+      yield x[i]
+      i = i + 1
+    end
+    binding.pry
+    x
 end
 
 
